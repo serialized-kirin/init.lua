8d16a68ff465915f6ff52a178c923519c8355d51
serialized-kirin <67888548+serialized-kirin@users.noreply.github.com>
2024-03-24 13:00:06 -0400

    Switch to neomin

--- init.lua
+++ init.lua
@@ -1,274 +1,214 @@
+vim.loader.enable() -- supposed to improve startup times. (experimental)
+
+local path_package = vim.fn.stdpath('data') .. '/site/'
+local mini_path = path_package .. 'pack/deps/start/mini.nvim'
+if not vim.loop.fs_stat(mini_path) then
+  vim.cmd('echo "Installing `mini.nvim`" | redraw')
+  local minirepo = 'https://github.com/echasnovski/mini.nvim'
+  vim.fn.system({ 'git', 'clone', '--filter=blob:none', minirepo, mini_path })
+  vim.cmd('packadd mini.nvim | echo "Installed `mini.nvim`" | redraw')
+end
+
+require('mini.deps').setup({ path = { package = path_package } })
+local pkg = MiniDeps.add;
+local doNow, doLater = MiniDeps.now, MiniDeps.later
+
 vim.cmd([[
-  let mapleader = ' '
-  let maplocalleader = ' '
-  set number relativenumber signcolumn=yes
-  set tabstop=2 shiftwidth=2 expandtab smarttab autoindent
-  set breakindent
-  set updatetime=250 timeoutlen=300
-  " this makes noice.nvim not annoying when using mini.completion
-  " set completeopt+='menuone,noselect' 
-  set termguicolors mouse=
-  set winminheight=0
-  " navigation like how I'm used to w/ tabs instead of buffers.
-  nnoremap <silent> <Leader>b <CMD>bn<CR>
-  nnoremap <silent> <Leader>B <CMD>bN<CR>
-  nnoremap <Leader>f :e<Space>
-  nnoremap <Leader>h :tab help<Space>
-  " used to these in the :terminal from vim
+  set tabstop=2 shiftwidth=2 expandtab
+  " set fillchars+=lastline:. display=truncate
+  set relativenumber number
+  set signcolumn=yes
+  let mapleader=' '
+  let maplocalleader=' '
   tnoremap <C-W><C-W> <C-\><C-N><C-W><C-W>
-  tnoremap <C-W><Escape> <C-\><C-N>
-  " I $%&^&*ing hate `Man.lua`. Worst plugin in existence.
+  tnoremap <C-W><Esc> <C-\><C-N>
+  " actually execute the `man` command instead of `man.lua`.
   nnoremap K :<C-U>let g:MAN_SV=@m<CR>"myiw:sp term://man <C-R>m<CR><CMD>let @m=g:MAN_SV<CR>
   vnoremap K :<C-U>let g:MAN_SV=@m<CR>gv"my:sp term://man <C-R>m<CR><CMD>let @m=g:MAN_SV<CR>
-  " for some reason this just feels right
-  nnoremap <Tab> w
-  cabbrev git !git
-
-  " I need to figure out how to only show batt in the active statusline.
-  function Batt()
-    return system("echo -n `pmset -g ps | awk '/%/{print $3 $4}' | tr ';' ' ' | sed -e 's/ discharging//' -e 's/charging/⚡️/'`")
-  endfunc 
-  set statusline=%f%(\ %M%)%(\ %H%)%(\ %R%) 
-  set statusline+=\ line\ %l/%L
-  " set statusline+=%=
-  " set statusline+=%(\ %{Batt()}%)%<
-  set statusline+=%(\ %S%)
-  set showcmdloc=statusline
-  set laststatus=3 " one single global statusline
+  nnoremap <Leader>f :e<Space>
+  nnoremap <Leader>h :tab<Space>help<Space>
+  autocmd FileType make set noexpandtab
 ]])
 
-vim.keymap.set('n', '[d', vim.diagnostic.goto_prev)
-vim.keymap.set('n', ']d', vim.diagnostic.goto_next)
-vim.keymap.set('n', '<leader>e', vim.diagnostic.open_float)
-vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist)
+doNow(function()
+  pkg('rmehri01/onenord.nvim') -- colorscheme
+  require('onenord').setup()
+  local my_headers_present, my_headers = pcall(require, 'my.header')
+  require('mini.starter').setup({ header = my_headers_present and my_headers or nil })
+  require('mini.statusline').setup({
+    content = {
+      active = function()
+        local mode, mode_hl = MiniStatusline.section_mode({ trunc_width = 80 })
+        -- need gitsigns.nvim for this to work
+        local git_ready, git = pcall(MiniStatusline.section_git,{ trunc_width = 1000 })
+        local filename = MiniStatusline.section_filename({ trunc_width = 1000 })
 
--- highlight on yank
--- See `:help vim.highlight.on_yank()`
-local highlight_group = vim.api.nvim_create_augroup('YankHighlight', { clear = true })
-vim.api.nvim_create_autocmd('TextYankPost', {
-  callback = function()
-    vim.highlight.on_yank()
-  end,
-  group = highlight_group,
-  pattern = '*',
-})
+        -- if I `set cmdheight=0`, this'll be good.
+        local macro_reg_hl, macro_reg = 'DiagnosticInfo', vim.fn.reg_recording()
+        macro_reg = macro_reg ~= "" and '(recording @' .. macro_reg .. ')'
 
-local lazypath = vim.fn.stdpath 'data' .. '/lazy/lazy.nvim'
-if not vim.loop.fs_stat(lazypath) then
-  vim.fn.system {
-    'git',
-    'clone',
-    '--filter=blob:none',
-    'https://github.com/folke/lazy.nvim.git',
-    '--branch=stable', -- latest stable release
-    lazypath,
-  }
-end
-vim.opt.rtp:prepend(lazypath)
+        local hydra_ready, hydrastatus = pcall(require, 'hydra.statusline')
+        if hydra_ready and hydrastatus.is_active() then mode = hydrastatus.get_name() end
 
-require('lazy').setup({
-  'tpope/vim-sleuth',
-  --[[ GIT ]]
-  { 
-    'lewis6991/gitsigns.nvim', 
-    config = function() 
-      -- need to add some keymaps for gitsigns commands-- I've been using it much
-      -- more than i expected.
-      require('gitsigns').setup()
-    end 
-  },
-  --[[ COLORSCHEME ]]
-  {
-    'navarasu/onedark.nvim',
-    priority = 1000,
-    lazy = false,
-    config = function()
-      require('onedark').setup {
-        style = 'warm',
-      }
-      require('onedark').load()
-    end,
-  },
-  --[[ NAVIGATION ]]
-  {
-    'mrquantumcodes/bufferchad.nvim', 
-    opts = { mapping = '<Leader>l' },
-    lazy = true,
-    keys = { '<Leader>l' },
-  },
-  {
-    'nvim-treesitter/nvim-treesitter',
-    dependencies = { 'nvim-treesitter/nvim-treesitter-textobjects', },
-    build = ':TSUpdate',
-    config = function()
-      -- Defer Treesitter setup after first render to improve startup time of 'nvim {filename}'
-      vim.defer_fn(function()
-        require('nvim-treesitter.configs').setup {
-          auto_install = true,
-          sync_install = false,
-          ignore_install = {},
-          modules = {},
-          highlight = { enable = true },
-          indent = { enable = true },
-          incremental_selection = {
-            enable = true,
-            keymaps = {
-              init_selection = '<c-space>',
-              node_incremental = '<c-space>',
-              scope_incremental = '<c-s>',
-              node_decremental = '<M-space>',
-            },
-          },
-          textobjects = {
-            select = {
-              enable = true,
-              lookahead = true,
-              keymaps = {
-                -- You can use the capture groups defined in textobjects.scm
-                ['af'] = '@function.outer',
-                ['if'] = '@function.inner',
-                ['ac'] = '@class.outer',
-                ['ic'] = '@class.inner',
-              },
-            },
-            move = {
-              enable = true,
-              set_jumps = true,
-              goto_next_start = {
-                [']]'] = '@function.outer',
-              },
-              goto_next_end = {
-                [']['] = '@function.outer',
-              },
-              goto_previous_start = {
-                ['[['] = '@function.outer',
-              },
-              goto_previous_end = {
-                ['[]'] = '@function.outer',
-              },
-            }
-          },
-        }
-      end, 0)
-    end
-  },
-  --[[ DEBUGGING ]]
-  {
-    {
-      'mfussenegger/nvim-dap',
-      config = function()
-        vim.keymap.set('n', '<Leader>dc', function() require('dap').continue() end)
-        vim.keymap.set('n', '<Leader>db', function() require('dap').toggle_breakpoint() end)
-        -- s for step, n for next, f for finish
-        -- vim.keymap.set('n', '<Leader>dn', function() require('dap').step_over() end)
-        -- vim.keymap.set('n', '<Leader>ds', function() require('dap').step_into() end)
-        -- vim.keymap.set('n', '<Leader>df', function() require('dap').step_out() end)
-      end
-    },
-    { 'rcarriga/nvim-dap-ui', dependencies = { 'mfussenegger/nvim-dap' } },
-    { 'theHamsta/nvim-dap-virtual-text', dependencies = { 'mfussenegger/nvim-dap' } },
-    { 
-      'jay-babu/mason-nvim-dap.nvim', 
-      dependencies = { 'mfussenegger/nvim-dap', 'williamboman/mason.nvim' },
-      config = function()
-        require('mason').setup()
-        require ('mason-nvim-dap').setup({
-          automatic_installation = true,
-          ensure_installed = {'codelldb'},
-          handlers = {}, -- sets up dap in the predefined manner
+        return MiniStatusline.combine_groups({
+          { hl = mode_hl, strings = { mode } },
+          '%<',
+          { hl = 'MiniStatuslineFilename', strings = { filename } },
+          '%=',
+          '%S', { strings = { macro_reg } },
+          git_ready and { hl = 'MiniStatuslineDevinfo', strings = { git } } or nil,
         })
       end
     }
-  },
-  --[[ LSP ]]
-  --[[{ -- still not sure how to use this one
-    'RishabhRD/nvim-lsputils', lazy = false, 
-    config = true, 
-    dependencies = { 'RishabhRD/popfix' },
-  },]]
-  {
-    'neovim/nvim-lspconfig',
-    dependencies = {
-      { 'williamboman/mason.nvim', config = true },
-      'williamboman/mason-lspconfig.nvim',
-      { 'j-hui/fidget.nvim', config = true },
-    },
-    config = function()
-      local on_attach = function(_, bufnr)
-        local nmap = function(keys, func, desc)
-          desc = desc and 'LSP: ' .. desc
-          vim.keymap.set('n', keys, func, { buffer = bufnr, desc = desc })
-        end
+  })
+  vim.cmd('set showcmdloc=statusline')
+  vim.cmd('set noshowmode')
+end)
 
-        nmap('<leader>rn', vim.lsp.buf.rename, '[R]e[n]ame')
-        vim.keymap.set({ 'n', 'v' }, '<Leader>ca', vim.lsp.buf.code_action, {})
-
-        nmap('<leader>K', vim.lsp.buf.hover, 'Hover Documentation')
-        nmap('<C-k>', vim.lsp.buf.signature_help, 'Signature Documentation')
-
-        nmap('gd', vim.lsp.buf.definition, '[G]oto [D]efinition')
-        -- set up simplistic two-step completion at some point..
-
-        -- set up simplistic two-step completion at some point..
-        -- vim.keymap.set('i', '<C-N>', '<C-X><C-O>')
-        -- ill just make omnifunc access faster instead for now
-        vim.keymap.set('i', '<C-X><C-X>', '<C-X><C-O>')
+doLater(function()
+  require('mini.pick').setup()
+  vim.keymap.set('n', '<Leader><Leader>', MiniPick.builtin.buffers)
+  vim.keymap.set('n', '<Leader>b', ':bn<CR>')
+  vim.keymap.set('n', '<Leader>B', ':bN<CR>')
+  vim.api.nvim_create_autocmd({ 'BufLeave', 'ExitPre', --[['BufUnload', 'VimLeavePre']] }, {
+    callback = function()
+      local curr_buf = vim.fn.bufnr('%')
+      for m in string.gmatch('ABCDEFGHIJKLMNOPQRSTUVWXYZ', '.') do
+        local buf = vim.api.nvim_get_mark(m, {})[3]
+        if buf == curr_buf then vim.fn.setpos("'"..m, vim.fn.getpos('.')) end
       end
-
-      -- mason-lspconfig requires that these setup functions are called in this order
-      -- before setting up the servers.
-      require('mason').setup()
-      require('mason-lspconfig').setup()
-
-      local servers = {
-        clangd = {},
-      }
-
-      local capabilities = vim.lsp.protocol.make_client_capabilities()
-
-      -- Ensure the servers above are installed
-      local mason_lspconfig = require 'mason-lspconfig'
-
-      mason_lspconfig.setup {
-        ensure_installed = vim.tbl_keys(servers),
-      }
-
-      mason_lspconfig.setup_handlers {
-        function(server_name)
-          require('lspconfig')[server_name].setup {
-            capabilities = capabilities,
-            on_attach = on_attach,
-            settings = servers[server_name],
-            filetypes = (servers[server_name] or {}).filetypes,
-          }
-        end,
-      }
     end
-  },
-  --    For additional information see: https://github.com/folke/lazy.nvim#-structuring-your-plugins
-  -- { import = 'custom.plugins' },
-}, 
-{
-    performance = { 
-      rtp = {
-        disabled_plugins = {
-          "gzip",
-          "editorconfig",
-          "health",
-          -- idk, i think doing this'll let me jump between start and end html tags again lol.
-          -- "matchit", 
-          -- highlights matching parens
-          -- "matchparen", 
-          "netrwPlugin",
-          "tarPlugin",
-          "tohtml",
-          "tutor",
-          "zipPlugin",
-          "man",
-          "spellfile",
-        },
-      }
+  })
+end)
+
+doLater(function()
+  pkg('numToStr/FTerm.nvim') -- floating terminal
+  require('FTerm').setup({ ft = 'FTerm', cmd = (os.getenv('SHELL') .. ' -il'), blend = 5 })
+  vim.api.nvim_create_autocmd('WinEnter', {
+    callback = function(ev) if(vim.bo[ev.buf].ft ~= 'FTerm') then require('FTerm').close() end end
+  })
+  vim.keymap.set('n', '<Leader>t', require('FTerm').toggle)
+end)
+
+doLater(function()
+  pkg('anuvyklack/hydra.nvim') -- making new modes
+  pkg('lewis6991/gitsigns.nvim') 
+  local Hydra = require("hydra")
+  local gitsigns = require('gitsigns')
+  local git_hint = [[
+  _j_: next hunk   _s_: stage hunk        _d_: show deleted   _b_: blame line
+  _k_: prev hunk   _u_: undo last stage   _p_: preview hunk   _B_: blame show full 
+  ^ ^              _S_: stage buffer      ^ ^                 _/_: show base file
+  ^
+  ^ ^              _<Esc>_: exit
+  ]]
+  gitsigns.setup() 
+  Hydra({
+    name = 'Git',
+    hint = git_hint,
+    config = {
+      buffer = false,
+      color = 'pink',
+      invoke_on_body = true,
+      hint = {
+        border = 'rounded'
+      },
+    },
+    mode = {'n','x'},
+    body = '<Leader>g',
+    heads = {
+      { 'j',
+        function()
+          if vim.wo.diff then return ']c' end
+          vim.schedule(function() gitsigns.next_hunk() end)
+          return '<Ignore>'
+        end,
+        { expr = true, desc = 'next hunk' } },
+      { 'k',
+        function()
+          if vim.wo.diff then return '[c' end
+          vim.schedule(function() gitsigns.prev_hunk() end)
+          return '<Ignore>'
+        end,
+        { expr = true, desc = 'prev hunk' } },
+      { 's', ':Gitsigns stage_hunk<CR>', { silent = true, desc = 'stage hunk' } },
+      { 'u', gitsigns.undo_stage_hunk, { desc = 'undo last stage' } },
+      { 'S', gitsigns.stage_buffer, { desc = 'stage buffer' } },
+      { 'p', gitsigns.preview_hunk_inline, { desc = 'preview hunk' } },
+      { 'd', gitsigns.toggle_deleted, { nowait = true, desc = 'toggle deleted' } },
+      { 'b', gitsigns.blame_line, { desc = 'blame' } },
+      { 'B', function() gitsigns.blame_line{ full = true } end, { desc = 'blame show full' } },
+      { '/', gitsigns.show, { exit = true, desc = 'show base file' } }, -- show the base of the file
+      { '<Esc>', nil, { exit = true, nowait = true, desc = 'exit' } },
     }
   })
+end)
 
--- vim: ts=2 sts=2 sw=2 et
+doLater(function()
+  pkg('nvim-treesitter/nvim-treesitter')
+  require('nvim-treesitter.configs').setup({
+    auto_install = true,
+    highlight = { enable = true },
+    indent = { enable = true },
+    textobjects = {
+      select = {
+        enable = true,
+        keymaps = {
+          ["af"] = "@function.outer",
+          ["if"] = "@function.inner",
+        }
+      },
+      move = {
+        enable = true,
+        set_jumps = true,
+        goto_next_start = { [']]'] = '@function.outer' },
+        goto_next_end = { [']['] = '@function.outer' },
+        goto_previous_start = { ['[['] = '@function.outer' },
+        goto_previous_end = { ['[]'] = '@function.outer' },
+      }
+    },
+  })
+  vim.cmd('set foldmethod=expr foldexpr=nvim_treesitter#foldexpr() nofoldenable')
+end)
+
+doLater(function()
+  pkg('williamboman/mason.nvim') -- nice interface for installing DAPs, LSPs, linters, etc.
+  pkg({
+    source = 'nvim-treesitter/nvim-treesitter-textobjects',
+    depends = { 'nvim-treesitter/nvim-treesitter' }
+  })
+  pkg({
+    source = 'williamboman/mason-lspconfig.nvim',
+    depends = { 'neovim/nvim-lspconfig' }
+  })
+  pkg('nvim-tree/nvim-web-devicons') -- for mini.statusline filetype icons
+
+  require('mini.completion').setup()
+  vim.cmd([[set completeopt+=menuone,noselect ]])
+
+  local lspconfig = require('lspconfig')
+  require('mason').setup({})
+  require("mason-lspconfig").setup({
+    handlers = { function(lsp) lspconfig[lsp].setup({}) end },
+    automatic_installation = true
+  })
+
+  vim.keymap.set('n', '<space>e', vim.diagnostic.open_float)
+  vim.keymap.set('n', '[d', vim.diagnostic.goto_prev)
+  vim.keymap.set('n', ']d', vim.diagnostic.goto_next)
+  vim.api.nvim_create_autocmd('LspAttach', {
+    group = vim.api.nvim_create_augroup('UserLspConfig', {}),
+    callback = function(ev)
+      vim.bo[ev.buf].omnifunc = 'v:lua.vim.lsp.omnifunc'
+      local opts = { buffer = ev.buf }
+      vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, opts)
+      vim.keymap.set('n', '<Leader>ca', vim.lsp.buf.code_action, opts)
+      vim.keymap.set('n', 'gd', vim.lsp.buf.definition, opts)
+      vim.keymap.set('n', '<Leader>K', vim.lsp.buf.hover, opts)
+      vim.keymap.set('n', '<Leader>gi', vim.lsp.buf.implementation, opts)
+      vim.keymap.set('n', '<Leader><C-k>', vim.lsp.buf.signature_help, opts)
+    end
+  })
+end)
+
+doLater(function() vim.cmd('helptags ALL') end)
